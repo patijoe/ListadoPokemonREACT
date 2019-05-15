ed44e9702b903fef118ffa9f90f819c5276472df
Patricia Garcia <patijoe33@gmail.com>
2019-05-15 11:31:22 +0200

    All is working

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="app">
+        <h1>Vamos allá</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
